43c35d1d42a4e9a63d2ed9bbfa7d5d653b1e5744
Annie Huang <annie.hn@gmail.com>
2022-12-10 21:30:45 +1100

    !!!! If you are at the beginning of a credit number field and you click ArrowLeft, it will move to the previous credit card input field.

--- script.js
+++ script.js
@@ -10,7 +10,32 @@ for (let i = currentYear; i < currentYear + 10; i++) {
 
 document.addEventListener('keydown', (e) => {
   const input = e.target;
+  const key = e.key;
   if (!isConnectedInput(input)) return;
+
+  /* selectionStart === 0 && input.selectionEnd === 0 means the cursor is at the start of the field
+     let's say you highlined the first 3 digits, then
+     selectionStart === 0 && input.selectionEnd === 3
+   */
+
+  switch (key) {
+    case 'ArrowLeft': {
+      // console.log(input.selectionStart);
+      // console.log(input.selectionEnd);
+
+      // If you are at the beginning of a credit number field and you click ArrowLeft, it will move to the previous credit card input field.
+      if (input.selectionStart === 0 && input.selectionEnd === 0) {
+        const prev = input.previousElementSibling;
+        prev.focus();
+        prev.selectionStart = prev.value.length - 1;
+        prev.selectionEnd = prev.value.length - 1;
+        e.preventDefault();
+      }
+      break;
+    }
+    default: {
+    }
+  }
 });
 
 function isConnectedInput(input) {
