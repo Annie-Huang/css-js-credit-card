45eea129739cd532afddce7a0ab3a1c675df48f0
Annie Huang <annie.hn@gmail.com>
2022-12-10 20:56:39 +1100

    !!! Add the next 10 year into the year field.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const expirationSelect = document.querySelector('[data-expiration-year]');
+
+const currentYear = new Date().getFullYear();
+for (let i = currentYear; i < currentYear + 10; i++) {
+  const option = document.createElement('option');
+  option.value = i;
+  option.innerText = i;
+  expirationSelect.append(option);
+}
